86400550fac12ccc8a7dfc3b7f9c88c4b828980d
Dorikan <ci06122004@gmail.com>
2025-10-02 04:52:20 +0300

    filtering nnpack warnings

--- ML-Service/main.py
+++ ML-Service/main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI
+import warnings
 
 from api.v1 import endpoints
 from utils.config import Config
@@ -8,4 +9,5 @@ app = FastAPI(title="ML Service", version="1.0")
 app.include_router(endpoints.router, prefix="/api/v1")
 
 if __name__ == "__main__":
+    warnings.filterwarnings("ignore", message="Could not initialize NNPACK")
     uvicorn.run(app, host=Config.API_HOST, port=Config.API_PORT)
